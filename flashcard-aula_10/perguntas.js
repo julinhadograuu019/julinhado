041bada49106109bcda623ac780c2e99ff3f5068
julinhadograuu019 <00001101243181sp@al.educacao.sp.gov.br>
2024-10-25 07:44:11 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cantor',
+    'Quem foi Luan Santana?',
+    'O Luan Rafael Domingos Santana é um cantor e compositor brasileiro. Iniciou sua carreira em 2009, com o lançamento do álbum independente Tô de Cara, e chegou ao estrelato com a canção "Meteoro".'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Paris?',
+    'A capital de Paris é França'
+)
+
+criaCartao(
+    'Qual o melhor jogo de Computador',
+    'O que é uma função?',
+    'O melhor jogo eletronico para computador é o Call of duty!!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz jogar vôlei em Inglês?',
+    'jogar vÔlei em inglês é play volleyball!'
+)
